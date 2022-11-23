0547c03a7a478c1d25ea92353c07b7fd19d3d9ef
FERREIRAR <romain.cdesafer@gmail.com>
2022-11-23 16:36:58 +0100

    16/11

---
+++ src/Fonctions/genererMDP.php
@@ -0,0 +1,38 @@
+<?php
+/*function passgen1($nbChar) {
+    $chaine ="mnoTUzS5678kVvwxy9WXYZRNCDEFrslq41GtuaHIJKpOPQA23LcdefghiBMbj0";
+    srand((double)microtime()*1000000);
+    $pass = '';
+    for($i=0; $i<$nbChar; $i++){
+        $pass .= $chaine[rand()%strlen($chaine)];
+    }
+    return $pass;
+}
+
+function passgen2($nbChar){
+    return substr(str_shuffle(
+        'abcdefghijklmnopqrstuvwxyzABCEFGHIJKLMNOPQRSTUVWXYZ0123456789'),1, $nbChar); }
+
+for ($i=0;$i<1000000;$i++){
+echo passgen1(10);
+    echo"\n";}
+
+echo passgen2(10);*/
+/*
+//Création de la séquence aléatoire à la base du mot de passe
+$octetsAleatoires = openssl_random_pseudo_bytes (6) ;
+//Transformation de la séquence binaire en caractères alpha
+$motDePasse = sodium_bin2base64($octetsAleatoires, SODIUM_BASE64_VARIANT_ORIGINAL);
+echo $motDePasse;
+echo PHP_EOL;*/
+function passgen1($nbChar)
+{
+    $chaine = "ABCDEFGHIJKLMONOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789&é\"'(-è_çà)=$^*ù!:;,~#{[|`\^@]}¤€";
+    srand(random_int(0, 999999999999999999));
+    $pass = '';
+    for ($i = 0; $i < $nbChar; $i++) {
+        $pass .= $chaine[rand() % strlen($chaine)];
+    }
+    return $pass;
+}
+    echo passgen1(10);
